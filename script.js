3ca5de2d890c958d38f0fb2e511aca4484316e92
abdelrahmanashour86-stack <abdelrahmanashour86@gmail.com>
2026-05-14 18:52:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const img = document.querySelector(".project-img")
+const github = document.querySelector(".link")
+const facebook = document.querySelector(".facebook")
+const whatsapp = document.querySelector(".whatsapp")
+const email = document.querySelector(".email")
+img.onclick =()=>{
+    window.open("https://abdelrahmanashour86-stack.github.io/bounty-radar-platform/")
+}
+github.onclick =()=>{
+    window.open("https://github.com/abdelrahmanashour86-stack/bounty-radar-platform")
+}
+facebook.onclick =()=>{
+    window.open("https://www.facebook.com/3bdelra7man3a4our")
+}
+whatsapp.onclick =()=>{
+    window.open("https://wa.me/201032039679")
+}
+email.onclick =()=>{
+    window.open("mailto:abdelrahmanashour86@gmail.com")
+}
