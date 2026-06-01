7cbc13d77f0200b9685e00d471c321adee7b722c
abdelrahmanashour86-stack <abdelrahmanashour86@gmail.com>
2026-06-01 20:57:07 +0300

    add project

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const projectsData = [
     {
         id: 1,
         type: "Dashboard",
+        title: "Bounty Radar (React Edition)",
+        description: "Bounty Radar Platform is a dynamic threat intelligence and bug bounty aggregator designed for security researchers. It seamlessly fetches live cybersecurity news, vulnerability write-ups, and hacker rankings into a sleek, Red Team-inspired dashboard. Re-architected with React.js, Vite, and Tailwind CSS v4, featuring a fully responsive UI and a seamless Dark/Light mode toggle.",
+        image: "bountyrada-react.png",
+        liveLink: "https://bounty-radar-react-edition.vercel.app/",
+        githubLink: "https://github.com/abdelrahmanashour86-stack/bounty-radar-react-edition"
+    },
+    {
+        id: 2,
+        type: "Dashboard",
         title: "bounty radar",
         description: "Bounty Radar Platform is a dynamic threat intelligence and bug bounty aggregator designed for security researchers. It seamlessly fetches live cybersecurity news, latest vulnerability write-ups, and global hacker rankings into a sleek, Red Team-inspired dashboard. Built entirely with Vanilla JavaScript, focusing on performance and clean architecture.",
         image: "bountyradar.png",
@@ -9,7 +18,7 @@ const projectsData = [
         githubLink: "https://github.com/abdelrahmanashour86-stack/bounty-radar-platform"
     },
     {
-        id: 2,
+        id: 3,
         type: "landing page",
         title: "botol",
         description: "A responsive and modern landing page built with a focus on clean design and optimal user experience. Developed using core front-end technologies to ensure fast load times, cross-browser compatibility, and pixel-perfect UI across all devices.",
@@ -18,7 +27,7 @@ const projectsData = [
         githubLink: "https://github.com/abdelrahmanashour86-stack/botol-landing-page" 
     },
     {
-        id: 3,
+        id: 4,
         type: "landing page",
         title: "iron",
         description: "A sleek, high-performance landing page designed to convert visitors effectively. Engineered with best practices in front-end architecture, delivering a seamless and interactive interface while maintaining clean, maintainable code.",
